4daf8a7ee7b91730b68a048fd686e5b8b73461ac
Hugo Souza <hugoregos@gmail.com>
2021-01-15 17:13:55 -0300

    Swagger e Swagger UI adicionados

--- src/main/java/com/loteriaorange/loteriaorange/controllers/Loteria.java
+++ src/main/java/com/loteriaorange/loteriaorange/controllers/Loteria.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.loteriaorange.loteriaorange.models.Aposta;
 import com.loteriaorange.loteriaorange.services.LoteriaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 
 
 @RestController
 @RequestMapping(value = "/apostador")
+@Api(value="API Loteria")
+@CrossOrigin(origins="*")
 public class Loteria {
 
 	@Autowired
 	private LoteriaService lotService;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ApiOperation(value = "Retorna lista de apostas do usuário")
 	public ResponseEntity<?> find(@PathVariable("id") String email) {
 
 		List<Aposta> apostadas = lotService.buscar(email);
@@ -30,6 +37,7 @@ public class Loteria {
 		
 	}
 	@GetMapping(value= "/criaraposta/{id}")
+	@ApiOperation(value = "Cria uma aposta para um usuário")
 	public ResponseEntity<?> create(@PathVariable("id") String email) {
 		Aposta aposta = lotService.create(email);
 		return ResponseEntity.ok().body(aposta);
