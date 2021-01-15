531367a572e1130c6f6617f20dfe90744f8b1cc6
Hugo Souza <hugoregos@gmail.com>
2021-01-15 13:16:36 -0300

    Projeto criado

---
+++ src/main/java/com/loteriaorange/loteriaorange/LoteriaorangeApplication.java
@@ -0,0 +1,13 @@
+package com.loteriaorange.loteriaorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriaorangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteriaorangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loteriaorange/loteriaorange/LoteriaorangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loteriaorange.loteriaorange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriaorangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
